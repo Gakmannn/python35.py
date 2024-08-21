bf252185158a42ba7cc8f517a3acfad6bb9f2812
Gakmannn <gnn87@mail.ru>
2024-08-21 21:29:33 +0300

    dop

--- postgresql/main.py
+++ postgresql/main.py
@@ -7,7 +7,15 @@ async def main() -> None:
 
     # write your queries here
     # users = await prisma.query_raw('SELECT * FROM User WHERE id = ?', 836760821,)
-    users = await prisma.query_raw('SELECT * FROM public.user')
+    # users = await prisma.query_raw('INSERT INTO user(email, name) VALUES (?,?)', 'dsfsd1@dfs.ru', 'user1',)
+    # users = await prisma.query_raw('INSERT INTO user(email, name) VALUES (?,?)', 'dsfsd2@dfs.ru', 'user2',)
+    # users = await prisma.query_raw('INSERT INTO user(email, name) VALUES (?,?)', 'dsfsd3@dfs.ru', 'user3',)
+    # users = await prisma.query_raw('INSERT INTO user(email, name) VALUES (?,?)', 'dsfsd4@dfs.ru', 'user4',)
+    # users = await prisma.query_raw('INSERT INTO user(email, name) VALUES (?,?)', 'dsfsd41@dfs.ru', 'user4',)
+    
+    
+    # users = await prisma.query_raw("SELECT * FROM user WHERE NOT name='user4' ORDER BY id desc;")
+    users = await prisma.query_raw("SELECT * FROM user WHERE name LIKE '%er3%';")
 
     print(users)
     
